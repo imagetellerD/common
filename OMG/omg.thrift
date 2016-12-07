46102128474044f5a212805bc4d3a662ae7c6926
chutong <chutong@domob.cn>
2016-12-07 19:30:51 +0800

    添加诗歌生成接口

--- OMG/omg.thrift
+++ OMG/omg.thrift
@@ -1,5 +1,6 @@
 /** OMG服务*/
 include "omg_types.thrift"
+include "omg_common.thrift"
 
 namespace java cn.domob.omg.thrift
 namespace php Thrift_OMG
@@ -9,20 +10,16 @@ namespace py domob_thrift.omg
 service OmgService{
   omg_types.Test test(
     /** 测试 */
-    1: i64 ad_id
+    1: i64 id
   )
 
   /** 根据用户输入生成诗词*/
   string generatePoem(
 	/** 生成诗词标题*/
 	1: string title,
-	/** 生成诗词关键字*/
-	2: list<string> important_words,
-  )
-
-  /** 对输入描述进行分词*/
-  list<string> splitWordsForPoem(
-	/** 输入的描述语句*/
-	1: string sentence,
-  )
+	/** 图片标签*/
+	2: list<string> tags,
+	/** 图片描述*/
+	3: list<string> description
+  ) throws (1: omg_common.OmgException e)
 }
